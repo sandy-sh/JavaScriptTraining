868808685de96dbc4f42057508e0f248b971e2c5
Sandy Syarif <sandysyarifhartanto@gmail.com>
2025-12-25 16:12:18 +0700

    Make Truncate A String Algorithm

---
+++ TruncateAStringAlgorithm.js
@@ -0,0 +1,9 @@
+const str = "Absolutely Longer";
+const num = 3;
+
+function truncateString(str, num) {
+    return str.length <= num ? str : str.slice(0, num) + "...";
+}
+
+console.log(truncateString(str, num));
+
