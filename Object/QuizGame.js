92f25df5fc4214eb6a10bc905c6099ae100c9378
Sandy Syarif <sandysyarifhartanto@gmail.com>
2026-01-03 23:57:19 +0700

    Initialize QuizGame with question structure and placeholder functions

---
+++ Object/QuizGame.js
@@ -0,0 +1,37 @@
+const questions = [
+  { category: "",
+    question: "",
+    choices: ["", "", ""],
+    answer: "" },
+  { category: "",
+    question: "",
+    choices: ["", "", ""],
+    answer: "" },
+  { category: "",
+    question: "",
+    choices: ["", "", ""],
+    answer: "" },
+  { category: "",
+    question: "",
+    choices: ["", "", ""],
+    answer: "" },
+  { category: "",
+    question: "",
+    choices: ["", "", ""],
+    answer: "" }
+];
+
+
+function getRandomQuestion (questions) {
+  return answer;
+}
+
+function getRandomComputerChoice (choices) {
+  return answer;
+}
+
+function getResults (getRandomQuestion, getRandomComputerChoice) {
+
+}
+
+console.log(questions);
