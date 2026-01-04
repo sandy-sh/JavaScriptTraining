ea8398a10448b0fae26ee4f161732999cc185abe
Sandy Syarif <sandysyarifhartanto@gmail.com>
2026-01-05 00:51:15 +0700

    Add consonant count functionality to SentenceAnalyzer

--- Loops/SentenceAnalyzer.js
+++ Loops/SentenceAnalyzer.js
@@ -12,3 +12,18 @@ function getVowelCount(sentence) {
 
 const vowelCount = getVowelCount("Apples are tasty fruits");
 console.log(`Vowel Count: ${vowelCount}`);
+
+function getConsonantCount(sentence) {
+  const consonants = "bcdfghjklmnpqrstvwxyz";
+  let count = 0;
+
+  for (const char of sentence.toLowerCase()) {
+    if (consonants.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const consonantCount = getConsonantCount("Coding is fun");
+console.log(`Consonant Count: ${consonantCount}`);
