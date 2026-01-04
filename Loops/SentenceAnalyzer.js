9bf590cd6e0ab017de81676dd9802bed3b38867d
Sandy Syarif <sandysyarifhartanto@gmail.com>
2026-01-05 00:35:29 +0700

    Build Sentence Analyzer and make a  function to count how many vowel in sentence

---
+++ Loops/SentenceAnalyzer.js
@@ -0,0 +1,14 @@
+function getVowelCount(sentence) {
+  const vowels = "aeiou";
+  let count = 0;
+
+  for (const char of sentence.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const vowelCount = getVowelCount("Apples are tasty fruits");
+console.log(`Vowel Count: ${vowelCount}`);
