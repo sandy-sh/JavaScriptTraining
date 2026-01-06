c78e6b58247e6cdb9b29816a8238c798b228b4ae
Sandy Syarif <sandysyarifhartanto@gmail.com>
2026-01-06 17:42:56 +0700

    Refactor chunkArrayInGroups function to use slice for chunking

--- Loops/ChunkyMonkeyAlgorithm.js
+++ Loops/ChunkyMonkeyAlgorithm.js
@@ -1,9 +1,19 @@
+// function chunkArrayInGroups(arr, size) {
+//     for(let i = 0; i < arr.length; i ++) {
+//         let taken = arr.splice(i, size);
+//         arr.splice(i, 0, taken);
+//     }
+//     return arr;
+// }
+
 function chunkArrayInGroups(arr, size) {
-    for(let i = 0; i < arr.length; i ++) {
-        let taken = arr.splice(i, size);
-        arr.splice(i, 0, taken);
-    }
-    return arr;
+    const result = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        const chunk = arr.slice(i, i + size);
+        result.push(chunk);
+    }   
+    return result;
 }
 
 console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
