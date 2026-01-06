63cbc0412e32584dba42060965ca3b38e533d057
Sandy Syarif <sandysyarifhartanto@gmail.com>
2026-01-06 17:36:28 +0700

    Build Factorial Calculator

---
+++ Loops/FactorialCalculator.js
@@ -0,0 +1,17 @@
+const num = 5;
+
+function factorialCalculator(num) {
+    let result = 1;
+    
+    for (let i = 1; i <= num; i++) {
+        result *= i;
+    }
+    
+    return result;
+}
+
+
+const factorial = factorialCalculator(num);
+const resultMsg = `The factorial of ${num} is ${factorial}`;
+
+console.log(resultMsg);
