7cf44cc30f463f80b10bc2e7f0ca37ed6f47062e
Sandy Syarif <sandysyarifhartanto@gmail.com>
2026-01-12 23:39:42 +0700

    feat: Update user stories for pairElement function to clarify functionality

--- string-algorithms/pairElement.js
+++ string-algorithms/pairElement.js
@@ -2,10 +2,12 @@
 User Stories:
 
 1. You should have a dropElements function that accepts an array (arr) and a function (func) as arguments.
-2. The dropElements function should iterate through the array and remove elements 
-starting from the first one until func returns true for an element.
-3. The dropElements function should return the remaining elements in the array if the condition is met
-4. If the condition is never satisfied, it should return an empty array.
+2. The pairElement function should return a 2d array, where each inner array has two strings inside, 
+the first string is one base from the input, and the second string the paired base.
+3. When given A, the function should pair it with T.
+4. When given T, the function should pair it with A.
+5. When given C, the function should pair it with G.
+6. When given G, the function should pair it with C.
 */
 
 const pairs = {
