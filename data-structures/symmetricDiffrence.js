68c3b191739702b1189d0b89c319a3da5123f4fb
Sandy Syarif <sandysyarifhartanto@gmail.com>
2026-01-16 23:21:27 +0700

    fix: Add missing stories to symmetricDiffrence.js

--- data-structures/symmetricDiffrence.js
+++ data-structures/symmetricDiffrence.js
@@ -1,3 +1,13 @@
+/*
+User Stories:
+
+1. Your function diffArray should return an array.
+2. Your function should wake two arguments, both of which are arrays. 
+3. Your function should make use of the filter method.
+4. Your function should return the symmetric difference of the two arrays.
+5. Your function should return an empty array if there is no symmetric.
+*/
+
 function diffArray (arr1, arr2) {
     return [
         ...arr1.filter(x => !arr2.includes(x)),
