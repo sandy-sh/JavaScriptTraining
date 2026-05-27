dd53bbd500760c2cb602cce6f90299222e11ccd6
Sandy Syarif <sandysyarifhartanto@gmail.com>
2026-05-27 14:35:09 +0700

    feat: Add Smart Pantry Restocker with shipment parsing and pantry initialization

---
+++ projects/data-processing-system/smart-pantry-restocker/SmartPantryRestocker.js
@@ -0,0 +1,35 @@
+const rawData = [
+  "KHI-500 | Kopi Hitam 500gr | 100 | 2026 | dry ",
+  "KHI-250 | Kopi Hitam 250gr | 100 | 2026 | dry",
+  "KSU-250 | Kopi Susu 250gr | 50 | 2026 | dry",
+  "TCL-BOX | Teh Celup Kotak | 30 | 2027 | dry",
+  
+  "SND-1LT | Susu Nasi Dingin 1L | 20 | 2026 | chilled",
+  "YGH-STR | Yogurt Strawberry | 45 | 2026 | chilled",
+  "KEJ-BLK | Keju Blok | 15 | 2026 | chilled",
+
+  "GRL-001 | Garam Dapur Lembut | 50 | 2028 | ", 
+  "GUL-PAS | Gula Pasir Putih | 40 | 2029",
+
+  "MIE-GNG | Mie Goreng Instan | 0 | 2026 | dry",
+  "SRA-BTL | Sirup Rasa Coco |  | 2027 | dry",
+
+  "BRS-WHT | Beras Putih Cianjur | 10 | unknown | dry", 
+  "MTR-UNK | Minyak Tanpa Rabel | 25 | 2026 | "
+]
+
+function parseShipment(rawData) {
+
+}
+
+function planRestock(pantry, shipment) {
+
+}
+
+function groupByZone(actions) {
+
+}
+
+function clonePantry(pantry) {
+
+}
